e5045e1e6e21aed1d5a8dc20c291ae535935b0d8
roman <usankin.roma@gmail.com>
2023-01-24 14:12:25 +0400

    first commit

---
+++ js/app.js
@@ -0,0 +1,36 @@
+document.addEventListener('DOMContentLoaded', () => {
+    function modal(modal, modalActiveClass, triggers, modalClose) {
+        const triggers_ = document.querySelectorAll(triggers),
+            modal_ = document.querySelector(modal),
+            modalClose_ = document.querySelector(modalClose);
+
+        if (triggers_.length > 0) {
+            triggers_.forEach(item => {
+                item.addEventListener('click', () => {
+                    modal_.classList.add(modalActiveClass);
+                    document.body.style.overflow = 'hidden';
+                    document.body.style.marginRight = `${scrollWidth}px`;
+                });
+            });
+
+            modalClose_.addEventListener('click', () => {
+                modal_.classList.remove(modalActiveClass);
+                document.body.style.overflow = '';
+                document.body.style.marginRight = '0px';
+            });
+
+            modal_.addEventListener('click', (e) => {
+                if (e.target.classList.contains('modal__wrapper')) {
+                    modal_.classList.remove(modalActiveClass);
+                    document.body.style.overflow = '';
+                    document.body.style.marginRight = '0px';
+                }
+            });
+        }
+    }
+
+    // регистрация
+    modal('.modal-registration', 'modal--active', '[data-modal="modal-registration"]', '.modal-registration__closer');
+    // узнать подробнее
+    modal('.modal-info', 'modal--active', '[data-modal="modal-info"]', '.modal-info__closer');
+});
